7dc24be768c1b2a10db72b2a5f5c2f0ccee811ce
JBayona <jordavids_22@hotmail.com>
2017-08-23 19:33:44 -0500

    A Queue With Two Stacks

---
+++ Data Structures/aQueueWithTwoStacks.java
@@ -0,0 +1,72 @@
+/*
+https://www.hackerrank.com/challenges/ctci-queue-using-two-stacks
+*/
+
+/*
+A queue is an abstract data type that maintains the order in which elements were added to it, 
+allowing the oldest elements to be removed from the front and new elements to be added to the rear. 
+This is called a First-In-First-Out (FIFO) data structure because the first 
+element added to the queue (i.e., the one that has been waiting the longest) is always the first one to be removed.
+
+A basic queue has the following operations:
+
+Enqueue: add a new element to the end of the queue.
+Dequeue: remove the element from the front of the queue and return it.
+In this challenge, you must first implement a queue using two stacks. Then process  queries, 
+where each query is one of the following  types:
+
+1 x: Enqueue element  into the end of the queue.
+2: Dequeue the element at the front of the queue.
+3: Print the element at the front of the queue.
+*/
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+    public static class MyQueue<T> {
+        Stack<T> s1 = new Stack<T>();
+        Stack<T> s2 = new Stack<T>();
+
+        public void enqueue(T value) { // Push onto newest stack
+            if(s2.isEmpty()) s2.push(value);
+            else s1.push(value);
+        }
+
+        public T peek() {
+             return s2.peek();
+        }
+
+        public T dequeue() {
+            if(s2.size() > 1) return s2.pop();
+            T current = s2.pop();
+            while(!s1.isEmpty()){
+                s2.push(s1.pop());
+            }
+            return current;
+        }
+    }
+
+    
+    public static void main(String[] args) {
+        MyQueue<Integer> queue = new MyQueue<Integer>();
+        
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        
+        for (int i = 0; i < n; i++) {
+            int operation = scan.nextInt();
+            if (operation == 1) { // enqueue
+                queue.enqueue(scan.nextInt());
+            } else if (operation == 2) { // dequeue
+                queue.dequeue();
+            } else if (operation == 3) { // print/peek
+                System.out.println(queue.peek());
+            }
+        }
+        scan.close();
+    }
+}
